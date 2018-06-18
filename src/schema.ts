0f5964a87ff838f96f01c47ffd0a0df198d54c02
Michal Szorád <miso.szorad@gmail.com>
2018-06-18 17:14:51 +0200

    Add subscription to schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,12 +1,14 @@
 import { UserMutation, UserQuery } from './users/schema'
-import { PostMutation, PostQuery } from './posts/schema'
+import { PostMutation, PostQuery, PostSubscription } from './posts/schema'
 import { CommentMutation, CommentQuery } from './comments/schema'
 
 interface Query extends UserQuery, PostQuery, CommentQuery {}
 interface Mutation extends UserMutation, PostMutation, CommentMutation {}
+interface Subscription extends PostSubscription {}
 
 /** @graphql schema */
 export interface Schema {
   query: Query
   mutation: Mutation
+  subscription: Subscription
 }
