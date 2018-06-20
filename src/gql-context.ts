0e0b4c74ffc524fecdf7e2e322b32a3c47f0a2c1
Michal Szorád <miso.szorad@gmail.com>
2018-06-20 11:51:25 +0200

    Fix graphql context types

--- src/gql-context.ts
+++ src/gql-context.ts
@@ -1,8 +1,10 @@
 import { GraphQLContext } from './types'
+import { Request } from 'express'
+import { IDocUser } from './users/models'
 
-function generateContext(req: any): GraphQLContext {
+function generateContext(req: Request): GraphQLContext {
   const ipAddress = String(req.connection.remoteAddress)
-  const user = req.user || null
+  const user = <IDocUser>req.user || null
   const authenticated = !!user
   return { ipAddress, authenticated, user }
 }
